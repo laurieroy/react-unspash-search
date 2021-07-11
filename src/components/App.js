63484c703850a7bcda0e74daa8bf2173e30888be
lauretteroy <laurieroy.dev@gmail.com>
2021-07-11 16:08:39 -0700

    Add ImageList, pass deconstructed images

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,27 @@
 import React from "react";
 // import ReactDOM from "react-dom";
+import unsplash from "../api/unsplash";
 
-import axios from "axios";
-
+import ImageList from "./ImageList"
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    const response = await axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`,
-        },
-      })
-      console.log(response.data.results)
+  state = { images: [] };
+
+  onSearchSubmit =  async (term) => {
+    const response = await unsplash
+      .get("/search/photos", {
+        params: { query: term }
+      });
+
+      this.setState({images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
